418f02997d5f04530e6e4dff56b0702fb4cf08b7
salsabilahnur <aenisalsabil@gmail.com>
2022-10-28 23:15:22 +0700

    Update Awal Project

---
+++ application/views/templates/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Salsabilah</li>
+        <li>Nama Belakang : Aeni</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Ds. Munjungagung Rt.003/ Rw.005 Kec.Kramat Kab.Tegal, Jawa Tengah</li>
+    </ul>
+ 
+    <h4>Tempat Lahir</h4>
+     <ul type="none">
+         <li>Tegal</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+     <ul type="square">
+        <li>Bulutangkis</li>
+         <li>Catur</li>
+     </ul>
+</section>
