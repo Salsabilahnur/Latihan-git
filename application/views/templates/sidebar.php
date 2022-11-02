889fd54b0c3060723edce1e77b76a557aca1241d
salsabilahnur <aenisalsabil@gmail.com>
2022-11-02 17:16:48 +0700

    Update sidebar

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -28,7 +28,19 @@
             </div>
 
             <!-- Nav Item - Pages Collapse Menu -->
-            
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                    <i class="fas fa-fw fa-cog"></i>
+                    <span>Data Akademik</span>
+                </a>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Data Mahasiswa:</h6>
+                        <a class="collapse-item" href="<?= base_url('');?>">Data Diri Mahasiswa</a>
+                        <a class="collapse-item" href="cards.html">KRS</a>
+                    </div>
+                </div>
+            </li>
 
             <!-- Nav Item - Utilities Collapse Menu -->
             <li class="nav-item">
